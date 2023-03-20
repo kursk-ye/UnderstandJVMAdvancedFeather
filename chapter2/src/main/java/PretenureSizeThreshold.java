67c2a2f1b58931363cebcfc46c990fa32b39c5e4
kursk-ye <kursk.ye@gmail.com>
2023-03-20 14:07:47 +0800

    init

---
+++ chapter2/src/main/java/PretenureSizeThreshold.java
@@ -0,0 +1,8 @@
+public class PretenureSizeThreshold {
+    private static final int _1MB =1024 * 1024;
+
+    public static void testPreenureSizeThreshold(){
+        byte[] allocation;
+        allocation = new byte[4* _1MB];
+    }
+}
